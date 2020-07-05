2d5d97a8f24e5bddab7795e7e98ae7dbd6a4d94d
henrique-tavares <ricktavares76@gmail.com>
2020-07-05 20:38:08 -0300

    cpp - hamming feito

---
+++ cpp/hamming/hamming.cpp
@@ -0,0 +1,27 @@
+#include "hamming.h"
+#include <stdexcept>
+
+using namespace std;
+
+namespace hamming
+{
+    unsigned int compute(string dna_1, string dna_2)
+    {
+        unsigned int distance = 0;
+
+        if (dna_1.length() != dna_2.length())
+        {
+            throw domain_error("dna strings have different sizes");
+        }
+
+        for (string::size_type i = 0; i < dna_1.length(); i++)
+        {
+            if (dna_1[i] != dna_2[i])
+            {
+                distance++;
+            }
+        }
+
+        return distance;
+    }
+} // namespace hamming
---
+++ cpp/hamming/hamming.h
@@ -0,0 +1,11 @@
+#if !defined(HAMMING_H)
+#define HAMMING_H
+
+#include <string>
+
+namespace hamming
+{
+    unsigned int compute(std::string dna_1, std::string dna_2);
+} // namespace hamming
+
+#endif // HAMMING_H
