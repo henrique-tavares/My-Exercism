979d16929934c8fca1872e082a023d0ec7a38b2e
henrique-tavares <ricktavares76@gmail.com>
2020-07-08 18:26:31 -0300

    ruby - high scores

---
+++ ruby/high-scores/high_scores.rb
@@ -0,0 +1,23 @@
+class HighScores
+  attr_accessor :scores
+
+  def initialize(scores)
+    @scores = scores
+  end
+
+  def latest()
+    @scores.last
+  end
+
+  def personal_best()
+    @scores.max
+  end
+
+  def personal_top_three()
+    @scores.max(3)
+  end
+
+  def latest_is_personal_best?()
+    @scores.last == personal_best()
+  end
+end
