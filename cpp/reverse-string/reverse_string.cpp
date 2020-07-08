3f0300693e867fd663250acd2d5c132d51e97990
henrique-tavares <ricktavares76@gmail.com>
2020-07-08 00:36:04 -0300

    ...

--- cpp/reverse-string/reverse_string.cpp
+++ cpp/reverse-string/reverse_string.cpp
@@ -1,5 +1,4 @@
 #include "reverse_string.h"
-#include <iostream>
 
 using namespace std;
 
@@ -7,13 +6,6 @@ namespace reverse_string
 {
     string reverse_string(string s)
     {
-        string reverted;
-
-        for (auto it = s.rbegin(); it != s.rend(); it++)
-        {
-            reverted.push_back(*it);
-        }
-
-        return reverted;
+        return string(s.rbegin(), s.rend());
     }
 } // namespace reverse_string
