1fbb0d90e1952bd0c485596e712cc2be647879bc
henrique-tavares <ricktavares76@gmail.com>
2020-07-13 18:20:10 -0300

    ...

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,11 +1,3 @@
 pub fn is_leap_year(year: u64) -> bool {
-    if year % 4 == 0 {
-        if year % 100 == 0 {
-            year % 400 == 0
-        } else {
-            true
-        }
-    } else {
-        false
-    }
+    year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
 }
