bd3170d1a8b73ce7758e6dda59038b490a18174c
henrique-tavares <ricktavares76@gmail.com>
2020-07-17 21:57:57 -0300

    python - protein translation

---
+++ python/protein-translation/protein_translation.py
@@ -0,0 +1,36 @@
+from typing import List
+
+
+def proteins(strand: str) -> List[str]:
+    PROTEIN_TRANSLATION = {
+        "AUG": "Methionine",
+        "UUU": "Phenylalanine",
+        "UUC": "Phenylalanine",
+        "UUA": "Leucine",
+        "UUG": "Leucine",
+        "UCU": "Serine",
+        "UCC": "Serine",
+        "UCA": "Serine",
+        "UCG": "Serine",
+        "UAU": "Tyrosine",
+        "UAC": "Tyrosine",
+        "UGU": "Cysteine",
+        "UGC": "Cysteine",
+        "UGG": "Tryptophan",
+        "UAA": "STOP",
+        "UAG": "STOP",
+        "UGA": "STOP",
+    }
+
+    proteins: List[str] = []
+
+    for i in range(0, len(strand), 3):
+        rna_sequence = strand[i : i + 3]
+
+        if rna_sequence in PROTEIN_TRANSLATION.keys():
+            if PROTEIN_TRANSLATION[rna_sequence] == "STOP":
+                break
+
+            proteins.append(PROTEIN_TRANSLATION[rna_sequence])
+
+    return proteins
