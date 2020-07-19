e6799ea516783e9bfd0bb927a5ffb13855d9f075
henrique-tavares <ricktavares76@gmail.com>
2020-07-19 20:09:24 -0300

    python - perfect numbers

---
+++ python/perfect-numbers/perfect_numbers.py
@@ -0,0 +1,15 @@
+from typing import Tuple
+
+
+def classify(number: int) -> str:
+    if number <= 0:
+        raise ValueError("Not a natural number")
+
+    aliquot_sum: int = sum([factor for factor in range(1, number) if number % factor == 0])
+
+    if aliquot_sum > number:
+        return "abundant"
+    elif aliquot_sum < number:
+        return "deficient"
+    else:
+        return "perfect"
