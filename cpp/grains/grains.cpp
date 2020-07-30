db4b8e8280ce34f64befb9b9040718822c2d99eb
henrique-tavares <ricktavares76@gmail.com>
2020-07-30 18:49:44 -0300

    ...

--- cpp/grains/grains.cpp
+++ cpp/grains/grains.cpp
@@ -1,17 +1,14 @@
 #include "grains.h"
 
-#include <cmath>
-#include <limits>
-
 namespace grains
 {
     unsigned long long square(int num)
     {
-        return (unsigned long long)std::pow(2, num - 1);
+        return 1ULL << (num - 1); // 2^(num - 1)
     }
 
     unsigned long long total()
     {
-        return std::numeric_limits<unsigned long long>::max();
+        return (2ULL << 63) - 1; // 2^64 - 1
     }
 } // namespace grains
